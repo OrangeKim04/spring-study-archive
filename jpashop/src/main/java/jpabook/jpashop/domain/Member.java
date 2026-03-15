d194c012c6180a3f62ebbb5c027a5d05006138cb
OrangeKim04 <queemkimu@naver.com>
2026-03-16 01:59:29 +0900

    refactor: 최신 스프링 부트 버전에 맞춘 JPA 패키지 변경 및 테스트 보완
    - Member, MemberRepository: 스프링 부트 3.x 이상 호환을 위해 javax.persistence 대신 jakarta.persistence 사용
    - 패키지 구조 개선: Member 엔티티를 domain 패키지로 이동
    - MemberRepositoryTest: @Rollback(false)를 추가하여 테스트 종료 후에도 H2 DB에서 실제 데이터(INSERT 쿼리) 확인 가능하도록 수정

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,15 +1,12 @@
-package jpabook.jpashop;
+package jpabook.jpashop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
 public class Member {
-
     @Id @GeneratedValue
     private Long id;
     private String username;
