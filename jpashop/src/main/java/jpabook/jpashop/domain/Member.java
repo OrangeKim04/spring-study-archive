4b0aa74bfbee6e49a60856716468621546e43a17
OrangeKim04 <queemkimu@naver.com>
2026-03-17 02:04:32 +0900

    feat: 회원 및 배송 관련 도메인 엔티티 생성
    - Member(회원), Delivery(배송), DeliveryStatus(배송상태) 엔티티 추가
    - 공통으로 사용할 Address(주소) 값 타입(@Embeddable) 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,10 +4,22 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter @Setter
 public class Member {
+
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-    private String username;
+
+    private String name;
+
+    @Embedded
+    private Address adress;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 }
